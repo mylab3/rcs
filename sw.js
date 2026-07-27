648b0ddf15d0e63fc005b5e5caeacea3aadd7f71
mylab3 <nm63rc2@gmail.com>
2026-07-27 11:25:21 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'rcs-v1';
+const urlsToCache = [
+  '/rcs/',
+  '/rcs/index.html',
+  '/rcs/stazioni2.xml'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
